08d04b0c8df8383b4a6f9649c36812de97236e4c
tomas.hampejs <caerulus@gmail.com>
2024-02-21 11:42:25 +0000

    dev

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,16 +12,6 @@ import sys
 
 nltk.download('wordnet')
 
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
-
 
 
 def get_synset_from_id(synset_id):
